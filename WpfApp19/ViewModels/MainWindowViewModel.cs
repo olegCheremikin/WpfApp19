9cea7ae86dc60ea99e429d91565bdf3887bbdedb
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2022-02-01 16:01:58 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using WpfApp19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double lengthCircle;
+        public double LengthCircle
+        {
+            get => lengthCircle;
+            set
+            {
+                lengthCircle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            LengthCircle = Circle.Calculation(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Radius >= 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
